3fa939ddd0b6b54feb66120e92be08b31ebd534b
Carole Gérard <gerard.carole@skynet.be>
2023-03-28 17:18:14 +0200

    interceptor

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,10 +6,11 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { NotFoundComponent } from './components/not-found/not-found.component';
-import { HttpClientModule } from '@angular/common/http';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { LoginComponent } from './auth/login/login.component';
 import { RegisterComponent } from './auth/register/register.component';
 import { ReactiveFormsModule } from '@angular/forms';
+import { AuthInterceptor } from './auth/interceptors/auth.interceptor';
 
 @NgModule({
   declarations: [
@@ -26,7 +27,7 @@ import { ReactiveFormsModule } from '@angular/forms';
     HttpClientModule,      // Pour utiliser HttpClient dans nos services et pouvoir faire des requêtes
     ReactiveFormsModule    // Pour le dossier auth
   ],
-  providers: [],
+  providers: [/* interceptor */  { provide : HTTP_INTERCEPTORS, useClass : AuthInterceptor, multi : true } ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
